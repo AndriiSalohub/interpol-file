f413f13fb3653606b0d1480f442ad7796c6dc144
Andrii <andrejsalogub3@gmail.com>
2024-05-06 11:01:58 +0300

    Added validation for criminal editing form fields.

--- InterpolFile/Forms/CriminalEditForm.cs
+++ InterpolFile/Forms/CriminalEditForm.cs
@@ -20,12 +20,17 @@ namespace InterpolFile.Forms
             InitializeComponent();
 
             this.criminal = criminal;
+            PopulateFields();
+        }
+
+        private void PopulateFields()
+        {
             firstNameTextBox.Text = criminal.FirstName;
             lastNameTextBox.Text = criminal.LastName;
             hairColorTextBox.Text = criminal.HairColor;
             eyesColorTextBox.Text = criminal.EyeColor;
             heightUpDown.Value = criminal.Height;
-            birthDateTimePicker.Text = criminal.DateOfBirth;
+            birthDateTimePicker.Value = DateTime.Parse(criminal.DateOfBirth);
             birthPlaceTextBox.Text = criminal.BirthPlace;
             aliasTextBox.Text = criminal.Alias;
             distinguishingFeaturesTextBox.Text = criminal.DistinguishingFeatures;
@@ -37,19 +42,69 @@ namespace InterpolFile.Forms
 
         private void submitButton_Click(object sender, EventArgs e)
         {
-            criminal.LastKnownResidence = lastKnownPlaceTextBox.Text;
-            criminal.LastCase = lastCrimeTextBox.Text;
-            criminal.CriminalProfession = proffesionTextBox.Text;
-            criminal.DistinguishingFeatures = distinguishingFeaturesTextBox.Text;
-            criminal.Alias = aliasTextBox.Text;
-            criminal.DateOfBirth = birthDateTimePicker.Text;
-            criminal.BirthPlace = birthPlaceTextBox.Text;
-            criminal.Height = (int)heightUpDown.Value;
-            criminal.EyeColor = eyesColorTextBox.Text;
-            criminal.HairColor = hairColorTextBox.Text;
-            criminal.LastName = lastNameTextBox.Text;
+            if (ValidateFields())
+            {
+                UpdateCriminal();
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                this.DialogResult = DialogResult.None;
+            }
+        }
+
+        private bool ValidateFields()
+        {
+            var fieldsToValidate = new Dictionary<TextBox, Label>
+            {
+                { firstNameTextBox, firstNameErrorLabel },
+                { lastNameTextBox, lastNameErrorLabel },
+                { hairColorTextBox, hairColorErrorLabel },
+                { eyesColorTextBox, eyesColorErrorLabel },
+                { birthPlaceTextBox, birthPlaceErrorLabel },
+                { aliasTextBox, aliasErrorLabel },
+                { distinguishingFeaturesTextBox, distinguishingFeaturesErrorLabel },
+                { proffesionTextBox, proffesionErrorLabel },
+                { lastCrimeTextBox, lastCrimeErrorLabel },
+                { languagesTextBox, languagesErrorLabel },
+                { lastKnownPlaceTextBox, lastPlaceErrorLabel }
+            };
+
+            bool allFieldsValid = true;
+            foreach (var pair in fieldsToValidate)
+            {
+                if (!ValidateTextField(pair.Key, pair.Value))
+                {
+                    allFieldsValid = false;
+                }
+            }
+
+            return allFieldsValid;
+        }
+
+        private bool ValidateTextField(TextBox textBox, Label errorLabel)
+        {
+            bool isValid = !string.IsNullOrWhiteSpace(textBox.Text);
+            errorLabel.Visible = !isValid;
+            return isValid;
+        }
+
+        private void UpdateCriminal()
+        {
             criminal.FirstName = firstNameTextBox.Text;
+            criminal.LastName = lastNameTextBox.Text;
+            criminal.HairColor = hairColorTextBox.Text;
+            criminal.EyeColor = eyesColorTextBox.Text;
+            criminal.Height = (int)heightUpDown.Value;
+            criminal.DateOfBirth = birthDateTimePicker.Value.ToShortDateString();
+            criminal.BirthPlace = birthPlaceTextBox.Text;
+            criminal.Alias = aliasTextBox.Text;
+            criminal.DistinguishingFeatures = distinguishingFeaturesTextBox.Text;
+            criminal.CriminalProfession = proffesionTextBox.Text;
+            criminal.LastCase = lastCrimeTextBox.Text;
             criminal.LanguagesKnown = languagesTextBox.Text.Split(',').ToList();
+            criminal.LastKnownResidence = lastKnownPlaceTextBox.Text;
         }
     }
 }
